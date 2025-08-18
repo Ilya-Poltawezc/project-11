dee83322c878807742490d974eb160673f8ff402
Ilya Poltawezc <ilya.poltawezc@gmail.com>
2025-08-18 16:16:52 +0300

    section project, js module Hover + adaptive, section supported + adaptive

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 import HeaderDrop from "./HeaderDrop.js";
 import Button from "./Button.js";
 import Video from "./Video.js";
+import Hover from "./Hover.js";
 
 new HeaderDrop()
 new Button()
 new Video()
+new Hover()
