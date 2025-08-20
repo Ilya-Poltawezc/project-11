bbd1130dc96dbd75668235a0f3b9082f066bc8fa
Ilya Poltawezc <ilya.poltawezc@gmail.com>
2025-08-20 16:44:34 +0300

    section datails, footer + adaptive, end first page

---
+++ scripts/Validation.js
@@ -0,0 +1,34 @@
+class Validation {
+    selectors = {
+        root: '[data-js-form]',
+        label: '[data-js-form-label]',
+        input: '[data-js-form-input]',
+    }
+
+    stateClasses = {
+        requared: 'requared',
+    }
+
+    constructor() {
+        this.rootElement = document.querySelector(this.selectors.root)
+        this.labelElement = this.rootElement.querySelector(this.selectors.label)
+        this.inputElement - this.rootElement.querySelector(this.selectors.input)
+        this.bindEvents()
+    }
+
+    requaredForm = (event) => {
+        event.preventDefault()
+
+        if (this.inputElement.value.trim() === '') {
+            this.labelElement.classList.add(this.stateClasses.requared)
+        } else {
+            this.labelElement.classList.remove(this.stateClasses.requared)
+        }
+    }
+
+    bindEvents() {
+        this.rootElement.addEventListener('submit', this.requaredForm)
+    }
+}
+
+export default Validation
--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,10 @@ import HeaderDrop from "./HeaderDrop.js";
 import Button from "./Button.js";
 import Video from "./Video.js";
 import Hover from "./Hover.js";
+import Validation from "./Validation.js";
 
 new HeaderDrop()
 new Button()
 new Video()
 new Hover()
+new Validation()
